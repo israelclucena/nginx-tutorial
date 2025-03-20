3fbd4ae23d6f352cd068f2a85f6f712cfb32c926
Israel Lucena <israelclucena@gmail.com>
2025-03-20 13:35:57 +0000

    fix: removing unecessary code and identation

--- server/index.js
+++ server/index.js
@@ -3,10 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("I am the '/' endpoint")
-})
+  res.send("I am the '/' endpoint");
+});
 
-
-app.listen(7777, ()=>{
-    console.log('Server running on port 7777')
-})
+app.listen(7777, () => {
+  console.log("Server running on port 7777");
+});
